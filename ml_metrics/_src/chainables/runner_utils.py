5ebc1480327d5a0462585f23598791a5a6cde618
Gene Huang <jinhuang@google.com>
2024-03-15 14:56:00 -0700

    removes unused apis in runner_utils.
    PiperOrigin-RevId: 616250834

--- ml_metrics/_src/chainables/runner_utils.py
+++ ml_metrics/_src/chainables/runner_utils.py
@@ -1,14 +1,11 @@
 """Utils for Chainable runners."""
 
-from collections.abc import Iterable
 import dataclasses
 import functools
-import itertools
-from typing import Any, Callable
+from typing import Any
 
 import cloudpickle as pickle
 from ml_metrics._src.chainables import lazy_fns
-from ml_metrics._src.chainables import transform as transform_lib
 
 
 # TODO: b/318463291 - support heterogeneous (de)serializations methods.
@@ -40,7 +37,7 @@ def _maybe_make_cached(payload: Any):
   return result
 
 
-# TODO: b/318463291 - Add unit test when ached is True.
+# TODO: b/318463291 - Add unit test when cached is True.
 def maybe_make(payload: Any, cached: bool = False):
   """Maybe deserialize a payload and call make when applicable."""
   if cached:
@@ -60,80 +57,3 @@ def clear_cache():
 def cache_info():
   """Returns the cache info for maybe_make."""
   _maybe_make_cached.cache_info()
-
-
-def _pickled(cached_args=None):
-  """This (de)serializes and instantiate function inputs and outputs.
-
-  This wraps in-process function to handle serialized inputs and serialize the
-  function outputs. The inputs are either depickled when the are bytes, and/or
-  instantiated if tthey are registered in lazy_fns.makeables.
-
-  Args:
-    cached_args: this will dicate which named args will be cached.
-
-  Returns:
-    The decorator handling (de)serialization of the inputs and outputs.
-  """
-  cached_args = cached_args or ()
-
-  def decorator(fn):
-    @functools.wraps(fn)
-    def wrapped(*args, **kwargs):
-      args = [maybe_make(v) for v in args]
-      kwargs = {
-          k: maybe_make(v, cached=(k in cached_args)) for k, v in kwargs.items()
-      }
-      result = fn(*args, **kwargs)
-      return picklers.default.dumps(result)
-
-    return wrapped
-
-  return decorator
-
-
-@_pickled(cached_args=('transform',))
-def run_update_state(
-    transform: transform_lib.CombinedTreeFn,
-    input_iterator: Iterable[Any],
-    initial_state=None,
-    num_steps: int | None = None,
-    record_num_steps: Callable[[int], None] | None = None,
-    record_state: Callable[[int, Any], None] | None = None,
-    record_busy: Callable[[bool], None] | None = None,
-):
-  """Run update_state of a transform for `num_steps` and returns the state."""
-  state = initial_state or transform.create_state()
-  if record_num_steps:
-    record_num_steps(-1)
-  if record_state:
-    record_state(0, state)
-  if record_busy:
-    record_busy(True)
-  for i, batch in enumerate(itertools.islice(input_iterator, num_steps)):
-    if i == 0 and record_num_steps:
-      record_num_steps(0)
-    state = transform.update_state(state, batch)
-    if record_num_steps:
-      record_num_steps(i + 1)
-    if record_state:
-      record_state(i, state)
-  if record_busy:
-    record_busy(False)
-  return state
-
-
-@_pickled(cached_args=('transform',))
-def run_merge_states(
-    transform: transform_lib.CombinedTreeFn,
-    states: list[Any],
-):
-  return transform.merge_states(states)
-
-
-@_pickled(cached_args=('transform',))
-def run_get_result(
-    transform: transform_lib.CombinedTreeFn,
-    state: Any,
-):
-  return transform.get_result(state)
