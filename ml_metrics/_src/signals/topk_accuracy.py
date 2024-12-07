02417e6746d14e50c1613e2dae171f25a3aa788d
ML Metrics Team <ml-metrics-dev@google.com>
2024-12-06 17:41:21 -0800

    Add normalizer to topk accuracy function.
    PiperOrigin-RevId: 703665672

--- ml_metrics/_src/signals/topk_accuracy.py
+++ ml_metrics/_src/signals/topk_accuracy.py
@@ -7,17 +7,22 @@ import numpy as np
 def topk_accurate(
     y_pred: types.NumbersT,
     label: int,
-    k: int,
+    weights: types.NumbersT = 1.0,
+    k: int = 1,
 ) -> bool:
   """Calculate topk accuracy.
 
   Args:
-    y_pred: prediction probabilities with shape [num_classes]
-    label: the ground truth label with values in [0, num_classes)
-    k: the top-k predictions to consider
+    y_pred: prediction scores with shape [num_classes].
+    label: the ground truth label with values in [0, num_classes).
+    weights: weight applied to the prediction scores for computing the
+      top-k accurate metric. Default is 1.0.
+    k: the top-k predictions to consider.
 
   Returns:
     True if the label is in the top-k predictions, False otherwise.
   """
-  topk_predictions = np.argsort(y_pred)[-k:]  # Get indices of top-k predictions
+  weighted_pred = np.asarray(y_pred) * np.asarray(weights)
+  # Get indices of top-k predictions
+  topk_predictions = np.argsort(weighted_pred)[-k:]
   return label in topk_predictions
--- ml_metrics/_src/signals/topk_accuracy_test.py
+++ ml_metrics/_src/signals/topk_accuracy_test.py
@@ -31,7 +31,40 @@ class TopkAccuracyTest(parameterized.TestCase):
       ),
   )
   def test_topk_accuracy(self, y_pred, label, k, expected):
-    self.assertEqual(topk_accuracy.topk_accurate(y_pred, label, k), expected)
+    self.assertEqual(topk_accuracy.topk_accurate(y_pred, label, k=k), expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="top_k_accuracy_weight_label1_k1_case0",
+          y_pred=[0.2, 0.7, 0.1],
+          label=1,
+          k=1,
+          weight=[1.0, 1.0/3.49, 1.0],
+          expected=1.0,
+      ),
+      dict(
+          testcase_name="top_k_accuracy_weight_label1_k1_case1",
+          y_pred=[0.2, 0.7, 0.1],
+          label=1,
+          k=1,
+          weight=[1.0, 1.0/3.51, 1.0],
+          expected=0.0,
+      ),
+      dict(
+          testcase_name="top_k_accuracy_weight_label0_k2",
+          y_pred=[0.2, 0.7, 0.1],
+          label=0,
+          k=2,
+          weight=[1.0, 1.0/3.51, 1.0],
+          expected=1.0,
+      ),
+  )
+  def test_topk_accuracy_with_weights(
+      self, y_pred, label, k, weight, expected
+  ):
+    self.assertEqual(
+        topk_accuracy.topk_accurate(y_pred, label, weight, k), expected
+    )
 
 
 if __name__ == "__main__":
