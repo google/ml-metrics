51b2caac0f1df8d5bfc3c8e894355b59d47ebdac
Gene Huang <jinhuang@google.com>
2025-01-08 14:58:09 -0800

    Adds ShardableDataSource type.
    PiperOrigin-RevId: 713427494

--- ml_metrics/_src/types.py
+++ ml_metrics/_src/types.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Google LLC
+# Copyright 2025 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 """Base types used throughout the library."""
 
 import abc
-from typing import Any, Protocol, TypeGuard, TypeVar, runtime_checkable
+from typing import Any, Iterable, Protocol, TypeGuard, TypeVar, runtime_checkable
 from numpy import typing as npt
 
 _T = TypeVar('_T')
@@ -38,6 +38,14 @@ class Resolvable(Protocol[_T]):
     """Interface to get the result of the underlying value."""
 
 
+class Shardable(Protocol[_T]):
+  """A sharded data source for chainables."""
+
+  @abc.abstractmethod
+  def iter_shard(self, shard_index: int = 0) -> Iterable[_T]:
+    """Iterates the data source given a shard index and number of shards."""
+
+
 MaybeResolvable = Resolvable[_T] | _T
 
 
@@ -53,6 +61,12 @@ def is_makeable(obj: Makeable[_T] | Any) -> TypeGuard[Makeable[_T]]:
   make = getattr(obj, 'make', None)
   # Also distinguish between classmethod or instance method.
   return make and getattr(make, '__self__', None) is obj
+
+
+def is_shardable(obj: Shardable[_T] | Any) -> TypeGuard[Shardable[_T]]:
+  """Checks if the object is a Shardable."""
+  shardable = getattr(obj, 'shard', None)
+  return shardable and getattr(shardable, '__self__', None) is obj
 
 
 def is_array_like(obj: list[Any] | tuple[Any, ...] | npt.ArrayLike) -> bool:
