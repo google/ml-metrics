4683e762f8f7278f58e10cb99df7653ef7788918
Gene Huang <jinhuang@google.com>
2025-05-11 11:31:53 -0700

    Add Count() aggregate.
    PiperOrigin-RevId: 757460899

--- ml_metrics/_src/aggregates/rolling_stats_test.py
+++ ml_metrics/_src/aggregates/rolling_stats_test.py
@@ -600,6 +600,44 @@ class MeanAndVarianceTest(parameterized.TestCase):
       except AssertionError:
         self.fail(f'Failed to assert {key}: {value} == {got[key]}')
 
+  def test_count_normal(self):
+    self.assertEqual(3, rolling_stats.Count()(['a', 'b', 'c']))
+
+  def test_count_merge(self):
+    count1 = rolling_stats.Count()
+    count2 = rolling_stats.Count()
+    count1.add(['a', 'b', 'c'])
+    count2.add(['a', 'b', 'c'])
+    count1.merge(count2)
+    self.assertEqual(6, count1.result())
+
+  def test_count_multi_column_inputs(self):
+    # Simulate two column of inputs that requires a batch_score_fn to process.
+    batches = [(1, 1), (2, 2)]
+    count = rolling_stats.Count(batch_score_fn=lambda *batch: list(batch))
+    for batch in batches:
+      count.add(*batch)
+    self.assertEqual(4, count.result())
+
+  def test_count_as_agg_fn(self):
+    p = (
+        transform.TreeTransform()
+        .batch(2)
+        .agg(rolling_stats.Count().as_agg_fn())
+    )
+    # auto-batch for single element
+    self.assertEqual(3, p.make()(input_iterator=range(3)))
+
+  def test_count_str(self):
+    count = rolling_stats.Count()
+    count.add(['a', 'b', 'c'])
+    self.assertEqual('count: 3', str(count))
+
+  def test_count_multi_column_no_batch_score_fn_raise(self):
+    count = rolling_stats.Count()
+    with self.assertRaisesRegex(ValueError, 'inputs requires a batch_score_fn'):
+      count.add(1, 2)
+
   def test_mean_normal(self):
     # This only tests that Mean().get_result() returns the mean value directly.
     state = rolling_stats.Mean().add([1, 2, 3])
