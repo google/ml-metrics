1a5a03b4ba3bda64154ae44fcb2ff9eb8c3fe755
ML Metrics Team <ml-metrics-dev@google.com>
2025-09-22 16:32:19 -0700

    *example to dict* 1. treat feature values as lists instead of single value if the feature only contains single scalar 2. support the conversion of list to ndarray with proper type to retain the feature type
    *dict to example*
    1. Check the type for numeric feature as they might be float instead of int which we could not rely on the first number of the list to determine the type.
    
    *move script to internal codebase*
    * Unit test requires using internal module (checked by our kokor check)
    
    PiperOrigin-RevId: 810197698

--- ml_metrics/_src/utils/proto_utils_test.py
+++ ml_metrics/_src/utils/proto_utils_test.py
@@ -11,91 +11,365 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from google.protobuf import text_format
 from ml_metrics._src.utils import proto_utils
 from ml_metrics._src.utils import test_utils
-import numpy as np
+import tensorflow as tf
 from absl.testing import absltest
 from absl.testing import parameterized
 from tensorflow.core.example import example_pb2
 
 
-def _get_tf_example(**kwargs):
-  example = example_pb2.Example()
-  for k, v in kwargs.items():
-    example.features.feature[k].bytes_list.value.append(v)
-    return example
-
-
-class TFExampleTest(parameterized.TestCase):
-
-  def test_single_example(self):
-    data = {
-        'bytes_key': b'\x80abc',  # not utf-8 decodable
-        'str_key': 'str_test',
-        'init_key': 123,
-        'np_int': np.int32(123),
-        'float_key': 4.56,
-        'np_float': np.float32(123),
+EXAMPLE_1 = text_format.Parse(
+    """
+features {
+  feature {
+    key: "bytes"
+    value {
+      bytes_list {
+        value: "ab"
+      }
     }
-    e = proto_utils.dict_to_tf_example(data).SerializeToString()
-    actual = proto_utils.tf_examples_to_dict(e)
-    self.assertDictAlmostEqual(data, actual, places=6)
-
-  def test_batch_example(self):
-    data = {
-        'bytes_key': [b'\x80abc', b'\x80def'],  # not utf-8 decodable
-        'str_key': ['str_test', 'str_test2'],
-        'init_key': [123, 456],
-        'np_int': [np.int32(123), np.int32(456)],
-        'float_key': [4.56, 7.89],
-        'np_float': [np.float32(123), np.float32(456)],
+  }
+  feature {
+    key: "bytes_arr"
+    value {
+      bytes_list {
+        value: "cd"
+        value: "ef"
+      }
     }
-    e = proto_utils.dict_to_tf_example(data)
-    actual = proto_utils.tf_examples_to_dict(e)
-    test_utils.assert_nested_container_equal(self, data, actual, places=6)
+  }
+  feature {
+    key: "int64"
+    value {
+      int64_list {
+        value: 1
+      }
+    }
+  }
+  feature {
+    key: "int64_arr"
+    value {
+      int64_list {
+        value: 2
+        value: 3
+      }
+    }
+  }
+  feature {
+    key: "float"
+    value {
+      float_list {
+        value: 1.5
+      }
+    }
+  }
+  feature {
+    key: "float_arr"
+    value {
+      float_list {
+        value: 2.5
+        value: 3.5
+      }
+    }
+  }
+}
+""",
+    example_pb2.Example(),
+)
+
+
+EXAMPLE_2 = text_format.Parse(
+    """
+features {
+  feature {
+    key: "bytes"
+    value {
+      bytes_list {
+        value: "mn"
+      }
+    }
+  }
+  feature {
+    key: "bytes_arr"
+    value {
+      bytes_list {
+        value: "op"
+        value: "qr"
+      }
+    }
+  }
+  feature {
+    key: "int64"
+    value {
+      int64_list {
+        value: 4
+      }
+    }
+  }
+  feature {
+    key: "int64_arr"
+    value {
+      int64_list {
+        value: 5
+        value: 6
+      }
+    }
+  }
+  feature {
+    key: "float"
+    value {
+      float_list {
+        value: 11.5
+      }
+    }
+  }
+  feature {
+    key: "float_arr"
+    value {
+      float_list {
+        value: 12.5
+        value: 13.5
+      }
+    }
+  }
+}
+""",
+    example_pb2.Example(),
+)
+
+
+class TFExampleTest(tf.test.TestCase, parameterized.TestCase):
 
   @parameterized.named_parameters(
       dict(
-          testcase_name='with_single_example',
-          num_elems=1,
+          testcase_name='tf_example',
+          example=EXAMPLE_1,
       ),
       dict(
-          testcase_name='multiple_examples',
-          num_elems=3,
+          testcase_name='serialized_tf_example',
+          example=EXAMPLE_1.SerializeToString(),
       ),
   )
-  def test_multiple_examples_as_batch(self, num_elems):
-    data = {
-        'bytes_key': b'\x80abc',  # not utf-8 decodable
-        'str_key': 'str_test',
-        'init_key': 123,
-        'np_int': np.int32(123),
-        'float_key': 4.56,
-        'np_float': np.float32(123),
+  def test_single_example_to_dict(self, example):
+    actual = proto_utils.tf_examples_to_dict(example)
+    expected = {
+        'bytes': [b'ab'],
+        'bytes_arr': [b'cd', b'ef'],
+        'int64': [1],
+        'int64_arr': [2, 3],
+        'float': [1.5],
+        'float_arr': [2.5, 3.5],
     }
-    e = [proto_utils.dict_to_tf_example(data) for _ in range(num_elems)]
-    actual = proto_utils.tf_examples_to_dict(e)
-    expected = {k: [v] * num_elems for k, v in data.items()}
     test_utils.assert_nested_container_equal(self, expected, actual, places=6)
 
-  def test_empty_example(self):
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='batch_tf_examples',
+          examples=[EXAMPLE_1, EXAMPLE_2],
+      ),
+      dict(
+          testcase_name='batch_serialized_tf_examples',
+          examples=[
+              EXAMPLE_1.SerializeToString(),
+              EXAMPLE_2.SerializeToString(),
+          ],
+      ),
+      dict(
+          testcase_name='batch_mixed_tf_examples',
+          examples=[EXAMPLE_1.SerializeToString(), EXAMPLE_2],
+      ),
+  )
+  def test_batched_examples_to_dict(self, examples):
+    actual = proto_utils.tf_examples_to_dict(examples)
+    expected = {
+        'bytes': [[b'ab'], [b'mn']],
+        'bytes_arr': [[b'cd', b'ef'], [b'op', b'qr']],
+        'int64': [[1], [4]],
+        'int64_arr': [[2, 3], [5, 6]],
+        'float': [[1.5], [11.5]],
+        'float_arr': [[2.5, 3.5], [12.5, 13.5]],
+    }
+    test_utils.assert_nested_container_equal(self, expected, actual, places=6)
+
+  def test_batched_single_example_to_dict(self):
+    actual = proto_utils.tf_examples_to_dict([EXAMPLE_1])
+    expected = {
+        'bytes': [[b'ab']],
+        'bytes_arr': [[b'cd', b'ef']],
+        'int64': [[1]],
+        'int64_arr': [[2, 3]],
+        'float': [[1.5]],
+        'float_arr': [[2.5, 3.5]],
+    }
+    test_utils.assert_nested_container_equal(self, expected, actual, places=6)
+
+  def test_missing_features_example_to_dict(self):
+    example_missing_features = text_format.Parse(
+        """
+    features {
+      feature {
+        key: "bytes"
+        value {
+          bytes_list {
+            value: "xy"
+          }
+        }
+      }
+    }
+    """,
+        example_pb2.Example(),
+    )
+
+    with self.assertRaisesRegex(
+        ValueError, 'All examples must have the same features'
+    ):
+      _ = proto_utils.tf_examples_to_dict([example_missing_features, EXAMPLE_1])
+
+  def test_empty_example_to_dict(self):
     self.assertEmpty(proto_utils.tf_examples_to_dict([]))
 
-  def test_unsupported_type(self):
-    with self.assertRaisesRegex(TypeError, 'Unsupported type'):
-      proto_utils.tf_examples_to_dict('unsupported_type')
+  def test_dict_to_tf_example(self):
+    data = {
+        'bytes_scalar': b'a',
+        'str_scalar': 'b',
+        'int64_scalar': 1,
+        'flaot_scalar': 2.1,
+        'bytes_list': [b'cd', b'ef'],
+        'str_list': ['gh', 'ij'],
+        'int64_list': [2, 3],
+        'float_list': [1, 3.5],
+    }
+    expected = text_format.Parse(
+        """
+    features {
+      feature {
+        key: "bytes_scalar"
+        value {
+          bytes_list {
+            value: "a"
+          }
+        }
+      }
+      feature {
+        key: "str_scalar"
+        value {
+          bytes_list {
+            value: "b"
+          }
+        }
+      }
+      feature {
+        key: "int64_scalar"
+        value {
+          int64_list {
+            value: 1
+          }
+        }
+      }
+      feature {
+        key: "flaot_scalar"
+        value {
+          float_list {
+            value: 2.1
+          }
+        }
+      }
+      feature {
+        key: "bytes_list"
+        value {
+          bytes_list {
+            value: "cd"
+            value: "ef"
+          }
+        }
+      }
+      feature {
+        key: "str_list"
+        value {
+          bytes_list {
+            value: "gh"
+            value: "ij"
+          }
+        }
+      }
+      feature {
+        key: "int64_list"
+        value {
+          int64_list {
+            value: 2
+            value: 3
+          }
+        }
+      }
+      feature {
+        key: "float_list"
+        value {
+          float_list {
+            value: 1.0
+            value: 3.5
+          }
+        }
+      }
+    }
+    """,
+        example_pb2.Example(),
+    )
+    actual = proto_utils.dict_to_tf_example(data)
+    self.assertProtoEquals(expected, actual)
 
-  def test_unsupported_value_type(self):
+  def test_dict_to_tf_example_key_with_empty_list(self):
+    data = {'int64_list': 1, 'empty_list': []}
+    expected = text_format.Parse(
+        """
+    features {
+      feature {
+        key: "int64_list"
+        value {
+          int64_list {
+            value: 1
+          }
+        }
+      }
+    }
+    """,
+        example_pb2.Example(),
+    )
+    actual = proto_utils.dict_to_tf_example(data)
+    self.assertProtoEquals(expected, actual)
+
+  def test_dict_to_tf_example_bad_str_type(self):
+    data = {'str_arr': ['abc', b'def']}
+    with self.assertRaisesRegex(AssertionError, 'bad str type'):
+      _ = proto_utils.dict_to_tf_example(data)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='bytes',
+          data={'bad_type': [b'ab', 'cd']},
+      ),
+      dict(
+          testcase_name='float',
+          data={'bad_type': [1.0, 'a']},
+      ),
+      dict(
+          testcase_name='int64',
+          data={'bad_type': [1, 'b']},
+      ),
+  )
+  def test_dict_to_tf_example_inconsistent_types(self, data):
+    # This test is required as the logic to determine the type of the feature
+    # list is based on the first value of the list.
+    with self.assertRaises(Exception):
+      _ = proto_utils.dict_to_tf_example(data)
+
+  def test_dict_to_tf_example_unsupported_type(self):
+    data = {'bad_type': [example_pb2.Example()]}
     with self.assertRaisesRegex(
-        TypeError, 'Value for "a" is not a supported type'
+        TypeError, 'Values for "bad_type" is not a supported type.'
     ):
-      proto_utils.dict_to_tf_example({'a': [example_pb2.Example()]})
-
-  def test_multiple_examples_missing_key(self):
-    data = [{'a': 'a', 'b': 1}, {'b': 2}]
-    examples = [proto_utils.dict_to_tf_example(d) for d in data]
-    with self.assertRaisesRegex(ValueError, 'Missing keys'):
-      _ = proto_utils.tf_examples_to_dict(examples)
+      _ = proto_utils.dict_to_tf_example(data)
 
 
 if __name__ == '__main__':
